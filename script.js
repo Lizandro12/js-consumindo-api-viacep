02eaf005c35c71087542a4ca10cb22837969e18d
Slandio Soares <88685339+Lizandro12@users.noreply.github.com>
2023-03-28 20:52:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
-let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/');
+let consultaCEP = fetch('https://viacep.com.br/ws/01001250/json/').then(resposta => resposta.json()).then(r => {
+    if(r.erro){
+        throw Error('Esse CEP não existe!')
+
+    }else{
+        console.log(r)
+    }}).catch(erro => console.log(erro));
 
 console.log(consultaCEP);
