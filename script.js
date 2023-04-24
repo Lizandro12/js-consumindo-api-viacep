f9eea62a9c104a788a00fb486c278436008ec7c5
Slandio Soares <88685339+Lizandro12@users.noreply.github.com>
2023-04-24 21:56:16 +0100

    finalizacao d oo projeto

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 async function buscaEndereco(cep){
 
+    let mensagemErro = document.getElementById("erro");
+    mensagemErro.innerHTML = "";
+
     try{
 
     let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
@@ -9,9 +12,23 @@ async function buscaEndereco(cep){
         throw Error('CEP não Existe')
     }
 
+    let cidade = document.getElementById("cidade");
+    let logradouro = document.getElementById("endereco");
+    let estado = document.getElementById("estado");
+
+    cidade.value = consultaCEPConvertida.localidade;
+    logradouro.value = consultaCEPConvertida.logradouro;
+    estado.value = consultaCEPConvertida.uf;
+
+
     console.log(consultaCEPConvertida);
     return consultaCEPConvertida
     } catch (erro){
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>` 
         console.log(erro)
     }
 }
+
+const cep = document.getElementById("cep");
+
+cep.addEventListener("focusout",()=> buscaEndereco(cep.value));
